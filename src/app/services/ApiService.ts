643f783d7d19d1633f75e9b69db2ea08c649eb67
Bobby Ekaro <iambobbymax@gmail.com>
2024-09-30 12:22:37 +0100

    updates

--- src/app/services/ApiService.ts
+++ src/app/services/ApiService.ts
@@ -27,7 +27,6 @@ export default class ApiService {
   constructor() {
     this.client = axios.create({
       baseURL: process.env.REACT_APP_BASE_URI,
-      withCredentials: true,
       headers: {
         "Content-Type": "application/json",
       },
