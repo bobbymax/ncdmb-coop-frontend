3915531b9bf582bba37e596811ed6fd3e49642c7
Bobby Ekaro <iambobbymax@gmail.com>
2024-10-04 11:35:38 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./assets/css/app.css";
 import { ContentProvider } from "./app/providers/ContentProvider";
