117d3c9b08bcdece4b9f919d19be3cf103a45bf0
Bobby Ekaro <iambobbymax@gmail.com>
2024-10-07 01:49:38 +0100

    updates

--- src/app/controllers/PlanController.ts
+++ src/app/controllers/PlanController.ts
@@ -2,5 +2,5 @@ import Controller from "./Controller";
 
 export default class PlanController extends Controller {
   public name: string = "PlanController";
-  public url: string = "users";
+  public url: string = "plans";
 }
